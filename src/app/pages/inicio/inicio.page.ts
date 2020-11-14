b08ae56ff2259187ba88b08071a1d575d39cc9d4
Angel Moreno <angel.moreno.romera@bbvaitspain.com>
2020-11-14 22:47:46 +0100

    ion-card

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -34,6 +34,11 @@ export class InicioPage implements OnInit {
       icon: 'radio-button-off–outline',
       name: 'Buttons',
       redirectTo: '/button'
+    },
+    {
+      icon: 'card-outline',
+      name: 'Cards',
+      redirectTo: '/card'
     }
   ];
 
